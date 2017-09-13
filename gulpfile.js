342e0b23aca3086c8c5a2fe6402b5a0cd09a3537
Bharatia <Vivek.Bharatia@emc.com>
2017-09-13 21:00:05 +0530

    changed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,6 @@ const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 
 require('./gulpfile-update-manifest');
+require('./gulpfile-upload-app-package');
 
 build.initialize(gulp);
